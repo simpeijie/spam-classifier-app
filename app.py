d5498f6163b8a4f4caf7734badcc1f12756821be
Pei Jie Sim <simpeijie@gmail.com>
2017-04-20 01:05:49 -0700

    Add support for generating random email

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField
 from wtforms.validators import Required
@@ -11,8 +11,26 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 bootstrap = Bootstrap(app)
 
+BASE_DIR = './data/'
+SPAM_DIR = 'spam/'
+HAM_DIR = 'ham/'
+
+import glob
+import random
+import codecs
+
+def getRandomEmail():
+	if random.random() >= 0.5:
+		filenames = glob.glob(BASE_DIR + HAM_DIR + '*.txt')
+	else:
+		filenames = glob.glob(BASE_DIR + SPAM_DIR + '*.txt')
+	n = random.randint(0, len(filenames))
+	filename = filenames[n]
+	with codecs.open(filename, encoding='latin-1') as f:
+		return f.read()
+
 class TextForm(FlaskForm):
-	text = TextAreaField('Email', validators=[Required()])
+	text = TextAreaField('Email', validators=[Required()], render_kw={"placeholder": "Input your own email or generate a random email."})
 	submit = SubmitField('Classify')
 
 import spam_classifier as spam
@@ -22,14 +40,17 @@ model, loss = spam.train_gd(X_train, y_train, 0.4, 0.1)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	form = TextForm()
 	res = None
 	email = ''
-	if form.validate_on_submit():
-		inputText = spam.generate_feature_vector(form.text.data)
-		res = spam.predict(inputText.dot(model), 0.9)[0]
-		email = form.text.data
+	form = TextForm()
+	if request.method == 'POST' and request.form['submit'] == 'Generate Random Email':
+		form.text.data = getRandomEmail()
+	elif request.method == 'POST':
+		if form.validate_on_submit():
+			email = form.text.data
+			inputText = spam.generate_feature_vector(email)
+			res = spam.predict(inputText.dot(model), 0.9)[0]
 	return render_template('index.html', form=form, res=res, email=email)
 
 if __name__ == '__main__':
-	app.run(debug=False)
+	app.run(debug=True)
